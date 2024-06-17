e189bfbb6c0009555d776155ca53e4f13e75c9d7
upasana0511 <bhattupasana1998@gmail.com>
2024-06-16 20:32:05 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <QuizComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuizComponent from './components/QuizComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QuizComponent
   }
-}
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  runtimeCompiler: true,
+  configureWebpack: {
+    plugins: []
+  },
+  chainWebpack: config => {
+    config.plugin('define').tap(definitions => {
+      definitions[0]['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = false
+      return definitions
+    })
+  }
+}
