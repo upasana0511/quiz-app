0cd0f723e7d20a9ba5149bb67df85a764c1e33d2
upasana0511 <bhattupasana1998@gmail.com>
2024-06-19 19:10:51 -0400

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,8 @@ module.exports = {
       definitions[0]['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = false
       return definitions
     })
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/quiz/'
+    : '/'
 }
